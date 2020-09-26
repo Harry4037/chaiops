0e5e21860d775e6b94f31a2c493b8e13cbfb6fa4
nitish <nitish@rizilianttech.com>
2020-09-26 10:09:36 +0530

    'admin'

--- database/seeds/ProductTypeTableSeeder.php
+++ database/seeds/ProductTypeTableSeeder.php
@@ -18,33 +18,33 @@ class ProductTypeTableSeeder extends Seeder {
          
             ],
                 [
-                'product_id' => 1,
+                'product_id' => 2,
                 'price' => 200,
                 'type' => '300ml',
                
             ],
                 [
-                'product_id' => 1,
+                'product_id' => 3,
                 'price' => 300,
                 'type' => '500ml',
             ],
                 [
-                'product_id' => 2,
+                'product_id' => 4,
                 'price' => 188,
                 'type' => '200ml',
             ],
                 [
-                'product_id' => 2,
+                'product_id' => 5,
                 'price' => 273,
                 'type' => '400ml',
             ],
                 [
-                'product_id' => 3,
+                'product_id' => 6,
                 'price' => 167,
                 'type' => '100ml',
             ],
                 [
-                'product_id' => 4,
+                'product_id' => 7,
                 'price' => 209,
                 'type' => '1Pc',
             ],
