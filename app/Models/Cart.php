d2be47e93cf1dedf542d21510b6b9f0457032c0d
nitish <nitish@rizilianttech.com>
2020-10-05 17:14:47 +0530

    'admin'

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model {
 
     public function product() {
-        return $this->belongsTo(Product::class, 'product_id');
+        return $this->belongsTo(Product::class, 'product_id')->withTrashed();
     }
     public function productType() {
-        return $this->belongsTo(ProductType::class, 'product_type_id');
+        return $this->belongsTo(ProductType::class, 'product_type_id')->withTrashed();
+    }
     }
 
 }
