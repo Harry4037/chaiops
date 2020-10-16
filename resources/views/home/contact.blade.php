a59ec50243e0bd0051a42eba2f77df38c97431ac
nitish <nitish3939@gmail.com>
2020-10-16 12:11:52 +0530

    'admin'

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -74,12 +74,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="mapwrapper row">
-                            <div class="">
-                                
-                                <iframe class="responsive-iframe"src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14014.064218808973!2d77.0628016697754!3d28.58429149999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1b29ff558a3d%3A0x342e71ccc8a77e3d!2sChaiops!5e0!3m2!1sen!2sin!4v1600871787763!5m2!1sen!2sin" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-                            </div>
-                        </div>
+                      
                     </div>
 
 
